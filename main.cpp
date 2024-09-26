554c44ede746ad13860aac6d31ea0afdf2f3987d
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-09-26 20:05:53 +0900

    save

--- main.cpp
+++ main.cpp
@@ -4,24 +4,21 @@
 #include <cmath>
 
 
-void PrintStickAngle(float x, float y) {
-    // スティックの入力から角度を計算
-    float angle = std::atan2(y, x);  // y, xの順で渡す
-
-    // 角度をラジアンから度に変換
-    float angleInDegrees = angle * (180.0f / 3.14159265359f);
-
-    // 結果を出力
-    std::cout << "Stick Input (x, y): (" << x << ", " << y << ")" << std::endl;
-    std::cout << "Angle (radians): " << angle << std::endl;
-    std::cout << "Angle (degrees): " << angleInDegrees << std::endl;
+void CalcForce(float level)
+{
+    float ans = 0.2f * float(std::pow(1.2f, level));
+    //std::cout << ans << std::endl;
+    printf("%.3f\n", ans);
 }
 
-int main() {
-    PrintStickAngle(1.0f, 0.0f);  // Right
-    PrintStickAngle(0.0f, 1.0f);  // Up
-    PrintStickAngle(-1.0f, 0.0f); // Left
-    PrintStickAngle(0.0f, -1.0f); // Down
+int main(void) {
+
+    CalcForce(0);
+    CalcForce(1);
+    CalcForce(2);
+    CalcForce(3);
+    CalcForce(4);
+    CalcForce(5);
 
     return 0;
 }
