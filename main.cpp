bf741369a9b86253d162eae1cbe9c0ae64f16935
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-10-26 00:20:23 +0900

    B153:蚊取線香

--- main.cpp
+++ main.cpp
@@ -1,53 +1,76 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 using namespace std;
 
-
-
 int main(void) {
+    // 一片の長さ
+    int length = 0;
+    cin >> length;
 
-	// 人数、試合数の入力
-	int totalPeople = 0, totalMatch = 0;
-	cin >> totalPeople; cin >> totalMatch;
+    // ブロック
+    vector<vector<int>> blocks(length, vector<int>(length));
+    for (int i = 0; i < length; ++i) {
+        for (int j = 0; j < length; ++j) {
+            cin >> blocks[i][j];
+        }
+    }
 
-	// 列車の初期状態
-	vector<string> mat(totalPeople, "1");
+    // それぞれの時間
+    vector<int> fractions(4, 0); // size, initValue
+    // トータル移動数
+    int totalMoveCount = length * length;
+    // 移動回数
+    int count = 0;
+    // 上下左右の境界
+    int up = 0, down = length - 1, left = 0, right = length - 1;
+    // 合計
+    int sum = 0;
 
-	for (int i = 0; i < totalMatch; ++i) {
-		// 勝敗の入力
-		int win = 0, lose = 0;
-		cin >> win; cin >> lose;
+    while (count < totalMoveCount) {
+        // 右に移動
+        for (int i = left; i <= right; i++) {
+            sum += blocks[up][i]; // 値を加算
+            count++; // 進行度
+            if (count % (totalMoveCount / 4) == 0) {
+                cout << sum << endl; // 1/4毎に出力
+            }
+        }
+        up++; // 上を一行走査済み
 
-		// 移動したのち、空にする
-		mat[win - 1] += mat[lose - 1];
-		mat[lose - 1].clear();
-	}
+        // 下に移動
+        for (int i = up; i <= down; i++) {
+            sum += blocks[i][right];
+            count++;
+            if (count % (totalMoveCount / 4) == 0) {
+                cout << sum << endl;
+            }
+        }
+        right--; // 右を一列走査済み
 
-	int maxLength = 0;
-	vector<int> longestIndices;
+        // 左に移動
+        if (up <= down) { // 境界を確認
+            for (int i = right; i >= left; i--) {
+                sum += blocks[down][i];
+                count++;
+                if (count % (totalMoveCount / 4) == 0) {
+                    cout << sum << endl;
+                }
+            }
+            down--; // 下を一行走査済み
+        }
 
-	for (int i = 0; i < mat.size(); ++i) {
-		const auto& element = mat[i];
-		int length = int(element.length());
-		
-		if (length > maxLength) {
-			maxLength = length;
-			longestIndices.clear();
-			longestIndices.push_back(i);
-		}
-		else if (length == maxLength) {
-			longestIndices.push_back(i);
-		}
-	}
+        // 上に移動
+        if (left <= right) { // 境界を確認
+            for (int i = down; i >= up; i--) {
+                sum += blocks[i][left];
+                count++;
+                if (count % (totalMoveCount / 4) == 0) {
+                    cout << sum << endl;
+                }
+            }
+            left++; // 左を一列走査済み
+        }
+    }
 
-	// 結果を表示
-	if (!longestIndices.empty()) {
-		
-		for (const int index : longestIndices) {
-			cout << index + 1 << endl;
-		}
-	}
-
-	return 0;
+    return 0;
 }
