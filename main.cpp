1411f15e9db99e43e7e796a2b75e6f3fec574b7e
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-10-25 22:19:19 +0900

    C016:Leet文字列

--- main.cpp
+++ main.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-int CountDonuts(const vector<string>& str, int h, int w) {
+string Convertor(const string& input) {
+    // Leet文字の対応表
+    unordered_map<char, char> leetMap = {
+        {'A', '4'}, {'E', '3'}, {'G', '6'}, {'I', '1'},
+        {'O', '0'}, {'S', '5'}, {'Z', '2'}
+    };
 
-	int count = 0;
-
-	// 少なくとも判定するのに1マス分余裕が必要なので
-	// 1 <= i <= h-2 && 1 <= j <= w-2 の範囲で探索
-	for (int i = 1; i < h - 1; ++i) {
-		for (int j = 1; j < w - 1; ++j) {
-			// 中心が空白なら
-			if (str[i][j] == '.') {
-				// 周囲のマスが全て黒ならcountを加算
-				if (str[i - 1][j - 1] == '#' && str[i - 1][j] == '#' && str[i - 1][j + 1] == '#' && // 上の行 
-					str[i][j - 1] == '#' && str[i][j + 1] == '#' && // 同じ行									 
-					str[i + 1][j - 1] == '#' && str[i + 1][j] == '#' && str[i + 1][j + 1] == '#') // 下の行
-				{
-					count++;
-				}
-			}
-		}
-	}
-
-	return count;
+    string resutl;
+    for (char str : input) {
+        // 対応表にあれば置き換える
+        if (leetMap.find(str) != leetMap.end()) {
+            resutl += leetMap[str];
+        }
+        else {
+            resutl += str;
+        }
+    }
+    return resutl;
 }
 
 int main(void) {
 
-	// マスの数
-	int h = 0, w = 0;
-	cin >> h; cin >> w;
-
-	// マスの中身
-	vector<string> strArray(h);
-
-	// 入力
-	for (int i = 0; i < h; ++i) {
-		cin >> strArray[i];
-	}
-
-	// ドーナツを見つける
-	int result = CountDonuts(strArray, h, w);
-	// 見つかった数を出力
-	cout << result << endl;
+    string input;
+    cin >> input;
+    string leetStr = Convertor(input);
+    cout << leetStr << endl;
 
 	return 0;
 }
