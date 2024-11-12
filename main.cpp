16f9570b5a9f55276cd2af7cd2cac748871dc51c
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-12 17:01:53 +0900

    save

--- main.cpp
+++ main.cpp
@@ -1,76 +1,51 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
 
-int main(void) {
-    // 一片の長さ
-    int length = 0;
-    cin >> length;
+struct VEC2 {
+    double x;
+    double y;
+};
 
-    // ブロック
-    vector<vector<int>> blocks(length, vector<int>(length));
-    for (int i = 0; i < length; ++i) {
-        for (int j = 0; j < length; ++j) {
-            cin >> blocks[i][j];
-        }
-    }
+int main()
+{
+    // 入力を受け取る
+    std::string input;
+    std::getline(std::cin, input);
+    // いったん出力
+    std::cout << input << std::endl;
 
-    // それぞれの時間
-    vector<int> fractions(4, 0); // size, initValue
-    // トータル移動数
-    int totalMoveCount = length * length;
-    // 移動回数
-    int count = 0;
-    // 上下左右の境界
-    int up = 0, down = length - 1, left = 0, right = length - 1;
-    // 合計
-    int sum = 0;
+    // 初期座標 原点座標 角度
+    VEC2 init;
+    VEC2 origin;
+    double angle;
 
-    while (count < totalMoveCount) {
-        // 右に移動
-        for (int i = left; i <= right; i++) {
-            sum += blocks[up][i]; // 値を加算
-            count++; // 進行度
-            if (count % (totalMoveCount / 4) == 0) {
-                cout << sum << endl; // 1/4毎に出力
-            }
-        }
-        up++; // 上を一行走査済み
+    // 値を取り出す
+    std::stringstream ss(input);
+    ss >> init.x >> init.y >> origin.x >> origin.y >> angle;
+    // いったん出力
+    std::cout << init.x << " " << init.y << std::endl;
+    std::cout << origin.x << " " << origin.y << std::endl;
+    std::cout << angle << std::endl;
 
-        // 下に移動
-        for (int i = up; i <= down; i++) {
-            sum += blocks[i][right];
-            count++;
-            if (count % (totalMoveCount / 4) == 0) {
-                cout << sum << endl;
-            }
-        }
-        right--; // 右を一列走査済み
+    // targetを求める
+    angle = angle * (3.1415926535897932 / 180); // 度数法->弧度法
+    VEC2 target = {
+        (init.x - origin.x) * std::cos(angle) - (init.y - origin.y) * std::sin(angle) + origin.x,
+        (init.x - origin.x) * std::sin(angle) + (init.y - origin.y) * std::cos(angle) + origin.y,
+    };
 
-        // 左に移動
-        if (up <= down) { // 境界を確認
-            for (int i = right; i >= left; i--) {
-                sum += blocks[down][i];
-                count++;
-                if (count % (totalMoveCount / 4) == 0) {
-                    cout << sum << endl;
-                }
-            }
-            down--; // 下を一行走査済み
-        }
+    // 小さい数を0として処理するための閾値
+    const double EPSILON = 1e-5;
 
-        // 上に移動
-        if (left <= right) { // 境界を確認
-            for (int i = down; i >= up; i--) {
-                sum += blocks[i][left];
-                count++;
-                if (count % (totalMoveCount / 4) == 0) {
-                    cout << sum << endl;
-                }
-            }
-            left++; // 左を一列走査済み
-        }
-    }
+    // 非常に小さい値を0として表示
+    if (std::abs(target.x) < EPSILON) target.x = 0.0;
+    if (std::abs(target.y) < EPSILON) target.y = 0.0;
+
+    std::cout << target.x << " " << target.y << std::endl;
 
     return 0;
 }
